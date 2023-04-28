f4b9796132745497f344b2dd51551241413d2907
garima <academy.learningshine@gmail.com>
2023-04-28 17:23:29 -0400

    Adding projects

---
+++ Display countries AJAX and RestAPI/script.js
@@ -0,0 +1,19 @@
+function showCountries() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      let countries = JSON.parse(this.response);
+      countries.forEach((country) => {
+        const countryCard = document.createElement("div");
+        const countryCardImage = document.createElement("img");
+
+        countryCard.innerHTML = country.name.common + "<br>";
+        countryCardImage.src = country.flags.png;
+        countryCard.appendChild(countryCardImage);
+        document.getElementById("feed").appendChild(countryCard);
+      });
+    }
+  };
+  xhr.send();
+}
