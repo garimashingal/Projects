1b657ac28d06d2cdb361f157b10ea45f94287f87
garima <academy.learningshine@gmail.com>
2023-06-13 11:14:16 -0400

    Styled Header (Inline style)

--- React Projects/phone-directory/src/Header.js
+++ React Projects/phone-directory/src/Header.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
 const Header = function () {
-  return <div className="header">Phone Directory</div>;
-};
+  const headerStyle = {
+    textAlign: "center",
+    padding: 20,
+    background: "#000",
+    color: "#fff",
+    textTransform: "uppercase",
+  };
 
-// class Header extends Component {
-//   render() {
-//     return <div className="header">Phone Directory</div>;
-//   }
-// }
+  return <div style={headerStyle}>Phone Directory</div>;
+};
 
 export default Header;
