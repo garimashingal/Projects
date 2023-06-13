66cdee9bf0541d1a4ef3e5360c2927a71567da7d
garima <academy.learningshine@gmail.com>
2023-06-13 16:26:19 -0400

    Rendered Subscribers Dynamically

--- React Projects/phone-directory/src/App.js
+++ React Projects/phone-directory/src/App.js
@@ -1,14 +1,39 @@
 import Header from "./Header";
+import "./App.css";
 
 function App() {
+  let subscribers = [
+    {
+      id: 1,
+      name: "Shilpa Bhatt",
+      phone: "2222222222",
+    },
+    {
+      id: 2,
+      name: "Bhavani Rai",
+      phone: "9999999999",
+    },
+  ];
   return (
     <div>
       <Header />
-      <button>Add</button>
       <div>
-        <span>Name</span>
-        <br />
-        <span>Phone</span>
+        <button className="add-button">Add</button>
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span>
+          <br />
+          <span className="grid-item phone-heading">Phone</span>
+        </div>
+
+        {subscribers.map((sub) => {
+          return (
+            <div key={sub.id} className="grid-container">
+              <span className="grid-item">{sub.name}</span>
+              <br />
+              <span className="grid-item">{sub.phone}</span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
