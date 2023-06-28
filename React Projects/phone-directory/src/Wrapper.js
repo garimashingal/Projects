ca04d10e5ec7ab49665f69c921b1c977293b7ac4
garima <academy.learningshine@gmail.com>
2023-06-28 17:19:37 -0400

    Delete unused code from files

--- React Projects/phone-directory/src/Wrapper.js
+++ React Projects/phone-directory/src/Wrapper.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import PhoneDirectory from "./PhoneDirectory";
 
 function Wrapprer() {
